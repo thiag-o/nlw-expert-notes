49148c8c5f4e9dc2cfa03345fc6ac5dbdf2e13b1
Thiago Borges Mansano <thiago@clubeagro.com.br>
2024-02-06 17:07:35 -0300

    ✅ feat: adicionado modal de exibir nota

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from "./assets/logo-nlw-expert.svg";
 import NewNoteCard from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+const note = {
+  date: new Date(),
+  content: "Hello World",
+};
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -18,9 +23,7 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   );
