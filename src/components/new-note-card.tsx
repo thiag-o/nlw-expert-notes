bd9583b54c96a229b00c0e77ff4f1ba5421726dd
Thiago Borges Mansano <thiago@clubeagro.com.br>
2024-02-07 19:36:13 -0300

    ✅ feat: adicionado speechRecognition para transcrever audio

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -6,8 +6,12 @@ import { toast } from "sonner";
 interface NewNoteCardProps {
   onNoteCreated: (content: string) => void;
 }
+
+let speechRecognition: SpeechRecognition | null = null;
+
 export default function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+  const [isRecording, setIsRecording] = useState(false);
   const [content, setContent] = useState("");
   function handleStartEditor() {
     setShouldShowOnboarding(false);
@@ -21,10 +25,54 @@ export default function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
+
+    if (content === "") {
+      return;
+    }
+
     onNoteCreated(content);
     setContent("");
     setShouldShowOnboarding(true);
     toast.success("Nota criada com suc  esso!");
+  }
+
+  function handleStartRecording() {
+    const isSpeechRecognitionApiAvailable =
+      "SpeechRecognition" in window || "webkitSpeechRecognition";
+
+    if (!isSpeechRecognitionApiAvailable) {
+      alert("Infelismente seu navegador não suporta a API de gravação!");
+    }
+    setIsRecording(true);
+    setShouldShowOnboarding(false);
+
+    const SpeechRecognitionAPI =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    speechRecognition = new SpeechRecognitionAPI();
+    speechRecognition.lang = "pt-BR";
+    speechRecognition.continuous = true;
+    speechRecognition.maxAlternatives = 1;
+    speechRecognition.interimResults = true;
+    speechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript);
+      }, "");
+      setContent(transcription);
+    };
+
+    speechRecognition.onerror = (event) => {
+      console.error(event);
+    };
+
+    speechRecognition.start();
+  }
+  function handleStopRecording() {
+    setIsRecording(false);
+
+    if (speechRecognition) {
+      speechRecognition.stop();
+    }
   }
 
   return (
@@ -44,7 +92,7 @@ export default function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
           <Dialog.Close className="absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
-          <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+          <form className="flex-1 flex flex-col">
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-sm font-medium text-slate-300">
                 Adicionar nota
@@ -52,12 +100,17 @@ export default function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
               {shouldShowOnboarding ? (
                 <p className="text-sm leading-6 text-slate-400">
                   Comece{" "}
-                  <button className="font-medium text-lime-400 hover:underline">
+                  <button
+                    onClick={handleStartRecording}
+                    type="button"
+                    className="font-medium text-lime-400 hover:underline"
+                  >
                     gravando uma nota
                   </button>{" "}
                   em áudio ou se preferir{" "}
                   <button
                     onClick={handleStartEditor}
+                    type="button"
                     className="font-medium text-lime-400 hover:underline"
                   >
                     utilize apenas texto
@@ -73,12 +126,24 @@ export default function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                 />
               )}
             </div>
-            <button
-              type="submit"
-              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-            >
-              Salvar nota
-            </button>
+            {isRecording ? (
+              <button
+                type="button"
+                onClick={handleStopRecording}
+                className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
+              >
+                <div className="size-3 rounded-full bg-red-500 animate-pulse" />
+                Gravando (clique p/ interromper)
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={handleSaveNote}
+                className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+              >
+                Salvar nota
+              </button>
+            )}
           </form>
         </Dialog.Content>
       </Dialog.Portal>
